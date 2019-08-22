2245fcad997c93f3ffe442a8707d0494404fb920
Sean Nguyen <sean.nguyenf@yahoo.com>
2019-08-22 16:41:43 -0400

    add Index and Note

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,11 +1,6 @@
 var express = require("express");
 var axios = require("axios");
 var cheerio = require("cheerio");
-var mongoose = require("mongoose")
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
 
 var router = express.Router();
 
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require("express");
+var logger = require("morgan");
 var exphbs = require("express-handlebars")
 var mongoose = require("mongoose");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 
 var PORT = process.env.PORT || 8000;
