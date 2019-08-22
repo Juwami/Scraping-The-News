f14dad5c1defb99f4994b539eb4e0ec4fdb390ab
Juwami <sean.nguyenf@yahoo.com>
2019-08-22 07:57:50 -0400

    receiving error with loading module models

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ var cheerio = require("cheerio");
 var PORT = process.env.PORT || 8000;
 var app = express();
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
