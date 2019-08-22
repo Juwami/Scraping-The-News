9e0e0126d4f49587c1f53ce6a7151cb5210e2dfa
Sean Nguyen <sean.nguyenf@yahoo.com>
2019-08-22 16:43:40 -0400

    removed express altogether and just exported it as an app

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,52 +1,52 @@
-var express = require("express");
 var axios = require("axios");
 var cheerio = require("cheerio");
-
-var router = express.Router();
-
 var db = require("../models");
 
-router.get("/", function (req, res) {
-    res.render("index")
+module.exports = function (app) {
+
+
+    
+    router.get("/", function (req, res) {
+        res.render("index")
 })
 
 router.get("/scrape", function (req, res) {
     axios.get("https://www.charlotteagenda.com/tag/open-houses/").then(function (response) {
         var $ = cheerio.load(response.data)
-
+        
         $("div.indexstory").each(function (i, element) {
             var result = {};
-
+            
             result.title = $(this)
-                .find("div.entry-item")
-                .find("h1.entry-title")
-                .find("a")
-                .text();
+            .find("div.entry-item")
+            .find("h1.entry-title")
+            .find("a")
+            .text();
             result.link = $(this)
-                .find("div.entry-item")
-                .find("h1.entry-title")
-                .find("a")
-                .attr("href");
+            .find("div.entry-item")
+            .find("h1.entry-title")
+            .find("a")
+            .attr("href");
             result.excerpt = $(this)
-                .find("div.excerpt.fullview")
-                .text();
+            .find("div.excerpt.fullview")
+            .text();
             result.postDate = $(this)
-                .find("div.entry-meta.fullview")
-                .find("div.byline-author")
-                .find("div.dateviews")
-                .find("div.bylinedate")
-                .text();
-
+            .find("div.entry-meta.fullview")
+            .find("div.byline-author")
+            .find("div.dateviews")
+            .find("div.bylinedate")
+            .text();
+            
             db.Article.create(result)
-                .then(function (house) {
-                    console.log(house)
-                })
-                .catch(function (err) {
-                    console.log(err)
-                })
+            .then(function (house) {
+                console.log(house)
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
         })
         res.send("Houses in Charlotte Scraped")
     })
 })
 
-module.exports = router
+}
