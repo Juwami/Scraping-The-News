9cb1b0776430fa5eb194221fb386e7d1be401045
Sean Nguyen <sean.nguyenf@yahoo.com>
2019-08-22 16:45:25 -0400

    requirement for axios and cheerio were moved from the server.js to controller.js

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ var logger = require("morgan");
 var exphbs = require("express-handlebars")
 var mongoose = require("mongoose");
 
-var axios = require("axios");
-var cheerio = require("cheerio");
-
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
