4650158cafed2ef0fdb5f300c3fd5b3e400a7305
Juwami <sean.nguyenf@yahoo.com>
2019-08-23 23:25:55 -0400

    make models static?

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.static("models"));
 
+app.use("/public", express.static(path.join(__dirname + "/public")));
+app.use("/models", express.static(path.join(__dirname + "/models")));
+
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
