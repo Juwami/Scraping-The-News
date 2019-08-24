c899f58a790c4726d6b3da3afffce456ab375c63
Juwami <sean.nguyenf@yahoo.com>
2019-08-23 23:43:38 -0400

    remove path and app use path

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var logger = require("morgan");
 var exphbs = require("express-handlebars")
 var mongoose = require("mongoose");
-var path = require("path")
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/CharlotteHomes";
 
@@ -15,9 +14,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.static("models"));
-
-app.use("/public", express.static(path.join(__dirname + "/public")));
-app.use("/models", express.static(path.join(__dirname + "/models")));
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
