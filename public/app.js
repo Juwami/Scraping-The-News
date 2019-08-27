72d2b17027bdca4150ae8447ed17a2aa92bc7423
Juwami <sean.nguyenf@yahoo.com>
2019-08-26 21:47:36 -0400

    add destroy button, removed jquery from bootstrap and it was not using ajax

--- public/app.js
+++ public/app.js
@@ -3,7 +3,13 @@ $("#saveBtn").on("click", function() {
     let id = $(this).data("id")
     console.log(id)
 
-    $.post("/articlesave/:id").then(function(data) {
+    $.post("/articlesave/:id").then(function() {
+        location.reload();
+    })
+})
+
+$("#destroyBtn").on("click", function() {
+    $.post("/destroy").then(function() {
         location.reload();
     })
 })
